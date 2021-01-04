7d376ee13950dcce6a9c3772b1eb212e02b4dfb7
FarisKadir <faris.kadir@gmail.com>
2021-01-03 20:05:13 -0500

    Creating arrays for letters, symbols, and numbers.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var length = prompt("How long do you want your password to be? Choose any number between 8 and 128. ");
-var upper = confirm("Should your password include uppercase letters? ");
-var lower = confirm("Should your password include lowercase letters? ");
-var symbols = confirm("Should your password include symbols? ");
-var numbers = confirm("Should your password include numbers? ");
+// Defining the password criteria
+var lengthChoice = prompt("How long do you want your password to be? Choose any number between 8 and 128. ");
+var upperChoice = confirm("Should your password include uppercase letters? ");
+var lowerChoice = confirm("Should your password include lowercase letters? ");
+var symbolChoice = confirm("Should your password include symbols? ");
+var numberChoice = confirm("Should your password include numbers? ");
 
+//Arrays for letters, symbols, and numbers.
+var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var symbols = ["`","~","!","@","#","$","%","^","&","*","(",")","?",",",".","{","}","[","]"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+function generatePassword() {
+
+  
+}
 
 
 // Write password to the #password input
