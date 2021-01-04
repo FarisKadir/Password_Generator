5b64c6cd6faca078bab6f9eec16cbc7459a4f824
FarisKadir <faris.kadir@gmail.com>
2021-01-03 19:51:37 -0500

    Defining variables for the password criteria in the js file.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = prompt("How long do you want your password to be? Choose any number between 8 and 128. ");
+var upper = confirm("Should your password include uppercase letters? ");
+var lower = confirm("Should your password include lowercase letters? ");
+var symbols = confirm("Should your password include symbols? ");
+var numbers = confirm("Should your password include numbers? ");
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
