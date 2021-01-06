07c438bbbc78442c661d5a3278eb7ea6be770b7b
FarisKadir <faris.kadir@gmail.com>
2021-01-05 23:37:17 -0500

    Finished the function that selects a random character from any of the array specified. I also included a function that shuffles the array so that there are no patterns in the password, ie. every 4th character is a symbol.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Pseudocode
+// //Step 1 pick a value from one of the arrays above, based on the user selection.
+//   //must only include arrays that are "true" based on the user confirmations.
+// //Step 2 add that value to an array for the password.
+// //Step 3 do this as many times as the length of the password which was chosen by the user.
+// //Step 4 Shuffle the array randomly. Use Durstenfield shuffle.
+// //Step 5 Return a concatentaion of all the array elements, without any delimiters.
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -13,7 +22,7 @@ var symbolChar = "";
 var numberChar = "";
 var confirmation = false;
 var randoArr = [];
-var randoPass = "";
+var password = "";
 
 
 //Arrays for uppercase and lowercase letters, symbols, and numbers.
@@ -41,12 +50,126 @@ function passwordCriteria()  {
 
 //Function that selects a random character from one of the password criteria arrays and places it in a temporary array.
 function randoChar(x)  {
-  if (randoPass < )
-  random = Math.floor(Math.random() * x.length);
-  char = x[random];
-  randoArr.push(char)
-  
+  if (password.length < lengthChoice) {
+    random = Math.floor(Math.random() * x.length);
+    char = x[random];
+    randoArr.push(char);
+    shuffle(randoArr);
+    password = randoArr.join("");
+  }
 }
+
+//Found this code that uses Fisher-Yates Shuffle to shuffle an array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function generatePassword()  {
+  for (var i = 0; i < lengthChoice; i++) {
+    i = i++;
+    if (upperChoice && lowerChoice && symbolChoice && numberChoice) {
+      randoChar(upperCase);
+      
+      randoChar(lowerCase);
+
+      randoChar(numbers);
+
+      randoChar(symbols);
+      
+    } else if (upperChoice && lowerChoice && numberChoice)  {
+        randoChar(upperCase);
+        
+        randoChar(lowerCase);
+
+        randoChar(numbers);
+      } else if (upperChoice && lowerChoice && symbolChoice)  {
+        randoChar(upperCase);
+        
+        randoChar(lowerCase);
+
+        randoChar(symbols);
+
+        } else if (upperChoice && symbolChoice && numberChoice)  {
+        randoChar(upperCase);
+        
+        randoChar(symbols);
+
+        randoChar(numbers);
+
+          } else if (symbolChoice && lowerChoice && numberChoice)  {
+            randoChar(symbols);
+            
+            randoChar(lowerCase);
+    
+            randoChar(numbers);
+            } else if (upperChoice && lowerChoice)  {
+              randoChar(upperCase);
+              
+              randoChar(lowerCase);
+              } else if (upperChoice && numberChoice)  {
+                randoChar(upperCase);
+                
+                randoChar(numbers);
+                } else if (upperChoice && symbolChoice)  {
+                  randoChar(upperCase);
+                  
+                  randoChar(symbols);
+                  } else if (lowerChoice && numberChoice)  {
+                    randoChar(lowerCase);
+                    
+                    randoChar(numbers);
+                    } else if (lowerChoice && symbolChoice)  {
+                      randoChar(lowerCase);
+                      
+                      randoChar(symbols);
+                      } else if (numberChoice && symbolChoice)  {
+
+                        randoChar(numbers);
+                        
+                        randoChar(symbols);
+
+                        } else if (upperChoice)  {
+
+                          randoChar(upperCase);
+
+                          } else if (lowerChoice)  {
+
+                            randoChar(lowerCase);
+                            
+                          } else if (numberChoice)  {
+
+                            randoChar(numbers);
+                            
+                          } else if (symbolChoice)  {
+
+                            randoChar(symbols);
+                            
+                          }
+  }
+
+  console.log(randoArr);
+  console.log(password);
+  console.log(password.length);
+
+  return password;
+}
+
+
 
 
 // Write password to the #password input
@@ -58,7 +181,6 @@ function writePassword() {
 }
 
 
-// console.log(randoPass);
 
 //Ask the user for the password length
 passwordLength();
@@ -92,46 +214,6 @@ while (confirmation === false)  {
 
 //Asks the user to click the Generate button to have the password created.
 window.alert("Click the 'Generate Password' button to create your password using the criteria you selected.");
-
-
-
-
-function generatePassword()  {
-  while (randoPass.length < lengthChoice) {
-    if (upperChoice && lowerChoice && symbolChoice && numberChoice) {
-      randoChar(upperCase);
-      
-      randoChar(lowerCase);
-
-      randoChar(numbers);
-
-      randoChar(symbols);
-
-      randoPass = randoPass.join("");
-
-      return randoPass;
-    }
-    else if (upperChoice && lowerChoice && numberChoice)  {
-      randoChar()
-    }
-  }
-}
-
-// for (var i = 0; i < lengthChoice.length; i++) {
-// //Step 1 pick a value from one of the arrays above, based on the user selection.
-//   //must only include arrays that are "true" based on the user confirmations.
-// //Step 2 add that value to an array for the password.
-// //Step 3 do this as many times as the length of the password which was chosen by the user.
-// //Step 4 Shuffle the array randomly. Use Durstenfield shuffle.
-// //Step 5 Return a concatentaion of all the array elements, without any delimiters.
-
-// }
-
-
-// return "this is your password";
-
-// }
-
 
 
 
